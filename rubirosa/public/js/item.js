aa83a74e0fb9f8a637b245028cdef6ce9200f1f5
lasalesi <larsericmueller@gmail.com>
2020-10-29 21:28:05 +0100

    Update item.js
    updated wait timeout

--- rubirosa/public/js/item.js
+++ rubirosa/public/js/item.js
@@ -1,19 +1,25 @@
 frappe.ui.form.on("Item", {
-    validate: function(frm) {
-        if ((!frm.doc.__islocal) && (frm.doc.is_stock_item)) {
-            send_to_msdirect(frm);
-        }
-    }
+    before_save: function(frm) {
+      if ((!frm.doc.__islocal) && (frm.doc.is_stock_item) && (frm.doc.barcode) && (frm.doc.barcode.length <= 17)) {
+          write_to_msdirect(frm);
+      }
 });
 
-function send_to_msdirect(frm) {
-    frappe.call({
-        "method": "rubirosa.rubirosa.msdirect.write_item",
-        "args": {
-            "item_code": frm.doc.item_code
-        },
-        "callback": function(response) {
-            frappe.show_alert( __("Sent to MS Direct") );
-        }
+function write_to_msdirect(frm) {
+    // wait for the data to be stored in the database
+    sleep(5000).then(() => {
+        frappe.call({
+    		method: 'rubirosa.rubirosa.msdirect.write_item',
+    		args: {
+    			'item_code': frm.doc.name
+    		},
+    		callback: function(r) {
+    			frappe.show_alert("Artikel an MS Direct übertragen");
+    		}
+    	}); 
     });
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
