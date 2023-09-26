d2790371c896f8fffdc88f108441ac16c8530b70
Valentina <vjss05@gmail.com>
2023-09-26 11:42:23 +0200

    Addition of: Verpackungseinheit, Kartonmenge, Volume, VK and Artikelart. Empty line removal in html. Error log for items that dont belong to selling RP EUR

--- rubirosa/rubirosa/doctype/multisped_settings/multisped_settings.py
+++ rubirosa/rubirosa/doctype/multisped_settings/multisped_settings.py
@@ -18,6 +18,7 @@ def get_items_data():
     sql_query = """
     SELECT
         IF(`mtr`.`item_code` IS NOT NULL, 'U', 'N') AS `item_status`,
+        `tabItem`.`name` AS `name`,
         `tabItem`.`name` AS `item_number`,
         `tabItem`.`description` AS `description`,
         `tabItem`.`item_group` AS `group`,
@@ -25,21 +26,29 @@ def get_items_data():
         MAX(IF(`tabItem Variant Attribute`.`attribute` = 'Colour', `tabItem Variant Attribute`.`attribute_value`, NULL)) AS `colour_value`,
         `tabItem`.`barcode` AS `ean_code`,
         `tabItem`.`stock_uom` AS `stock_uom`,
+        `uom`.`uom` AS `uom`,
+        `uom`.`conversion_factor` AS `kartonmenge`,
         `tabItem`.`weight_per_unit` AS `weight`,
+        `tabItem Price`.`price_list_rate` AS `vk`,
         `tabItem`.`country_of_origin` AS `origin`,
-        `tabItem`.`customs_tariff_number` AS `tariff`       
+        `tabItem`.`customs_tariff_number` AS `tariff`,
+        `tabItem Price`.`currency` AS `currency`      
     FROM
         `tabItem`
     LEFT JOIN
         `tabItem Variant Attribute` ON `tabItem Variant Attribute`.`parent` = `tabItem`.`name`
         AND (`tabItem Variant Attribute`.`attribute` = 'Size' OR `tabItem Variant Attribute`.`attribute` = 'Colour')
     LEFT JOIN `tabMultisped Transfer Record` AS `mtr` ON `tabItem`.`name` = `mtr`.`item_code`
+    LEFT JOIN `tabStock Ledger Entry` AS `sle` ON `tabItem`.`name` = `sle`.`item_code`
+    LEFT JOIN `tabUOM Conversion Detail` AS `uom` ON `tabItem`.`name` = `uom`.`parent`
     LEFT JOIN
-        `tabStock Ledger Entry` AS `sle` ON `tabItem`.`name` = `sle`.`item_code`
+        `tabItem Price` ON `tabItem Price`.`item_code` = `tabItem`.`name`
+        AND ( `tabItem Price`.`price_list` = 'Selling RP EUR')
     WHERE
         `tabItem`.`disabled` = 0
         AND `tabItem`.`is_sales_item` = 1
         AND `sle`.`actual_qty` > 0
+        AND `tabItem`.`barcode` IS NOT NULL
     GROUP BY
         `tabItem`.`name`
     ORDER BY
@@ -47,9 +56,25 @@ def get_items_data():
     """
     data = frappe.db.sql(sql_query, as_dict=True)
     
-    for row in data:
+    indices_to_remove = []
+    
+    for i, row in enumerate(data):
+		
         if row['item_number']:
             row['item_number'] = hashlib.md5(row['item_number'].encode('utf-8')).hexdigest()[:20]
+            
+        if row['weight']:
+            row['weight'] = ("{:,.2f}".format(row['weight'] or 0)).replace(".", ",")
+            
+        if row['vk'] is None:
+            frappe.log_error("Item: {0} Item Number: {1} has no price list'Selling RP EUR' ".format(row['name'],row['item_number']))
+            indices_to_remove.append(i)
+        else:
+            row['vk'] = ("{:,.2f}".format(row['vk'] or 0)).replace(".", ",")
+    
+    # Remove rows where price_list_rate/vk is None
+    for index in reversed(indices_to_remove):
+        data.pop(index)
     
     return data
 
@@ -81,13 +106,13 @@ def update_items_records(items):
         try:
             mtr = frappe.get_doc({
                 'doctype': 'Multisped Transfer Record',
-                'item_code': i['item_number']
+                'item_code': i['name']
             })
 
             mtr.insert(ignore_permissions=True)    
             frappe.db.commit()
             mtr_ref = mtr.name
         except Exception as e:
-            frappe.log_error("{0}\n\n{1}".format(e, i['item_number']), "Update Items Records Failed")
+            frappe.log_error("{0}\n\n{1}".format(e, i['name']), "Update Items Records Failed")
     
     return
