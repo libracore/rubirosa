9541fad889bc3fab8d13200ff2981c59c6392719
Valentina <vjss05@gmail.com>
2023-10-05 22:05:50 +0200

    Marketing Material retrival depending on orders or season

--- rubirosa/rubirosa/assets.py
+++ rubirosa/rubirosa/assets.py
@@ -9,16 +9,65 @@ import json
 
 @frappe.whitelist()
 def get_user_info(user):
+    orders = get_user_orders(user)
+    invoices = get_user_invoices(user)
+    marketing_material = get_marketing_material(orders)
+
+    user_info = orders + invoices
+
+    return {"user_info": user_info, "marketing_material": marketing_material}
+    
+def get_user_orders(user):
 
     sql_query = """
         SELECT 
-		`tabSales Order`.`name` AS `sales_orders`
-		FROM `tabContact`
-		LEFT JOIN `tabDynamic Link` ON `tabContact`.`name` = `tabDynamic Link`.`parent`
-			AND `tabDynamic Link`.`link_doctype` = "Customer"
-		LEFT JOIN `tabSales Order` ON `tabDynamic Link`.`link_name` = `tabSales Order`.`customer` AND `tabSales Order`.`docstatus` > 0
-		WHERE `tabContact`.`email_id` = '{user}'
+        `tabSales Order`.`name` AS `sales_orders`
+        FROM `tabContact`
+        LEFT JOIN `tabDynamic Link` ON `tabContact`.`name` = `tabDynamic Link`.`parent`
+            AND `tabDynamic Link`.`link_doctype` = "Customer"
+        LEFT JOIN `tabSales Order` ON `tabDynamic Link`.`link_name` = `tabSales Order`.`customer` AND `tabSales Order`.`docstatus` = 1
+        WHERE `tabContact`.`email_id` = '{user}'
     """.format(user=user)
-    data = frappe.db.sql(sql_query, as_dict = True)
+    orders = frappe.db.sql(sql_query, as_dict = True)
     
-    return data
+    return orders
+
+def get_user_invoices(user):
+
+    sql_query = """
+        SELECT 
+        `tabSales Invoice`.`name` AS `sales_invoices`
+        FROM `tabContact`
+        LEFT JOIN `tabDynamic Link` ON `tabContact`.`name` = `tabDynamic Link`.`parent`
+            AND `tabDynamic Link`.`link_doctype` = "Customer"
+        LEFT JOIN `tabSales Invoice` ON `tabDynamic Link`.`link_name` = `tabSales Invoice`.`customer` AND `tabSales Invoice`.`docstatus` = 1
+        WHERE `tabContact`.`email_id` = '{user}'
+    """.format(user=user)
+    invoices = frappe.db.sql(sql_query, as_dict = True)
+    
+    return invoices
+
+def get_marketing_material(orders):
+	
+    order_names = [order['sales_orders'] for order in orders]
+    orders_str = ', '.join(["'" + order + "'" for order in order_names])
+
+    sql_query = """
+        SELECT
+            `tabMarketing Material`.`season`,
+            `tabMarketing Material`.`item_code`,
+            `tabMarketing Material`.`image`
+        FROM
+            `tabMarketing Material`
+        INNER JOIN
+            `tabSales Order Item` ON `tabMarketing Material`.`item_code` = `tabSales Order Item`.`item_code`
+        INNER JOIN
+            `tabSales Order` ON `tabSales Order Item`.`parent` = `tabSales Order`.`name`
+        WHERE
+            `tabSales Order`.`name` IN ({orders})
+            OR `tabSales Order`.`sales_season` = `tabMarketing Material`.`season`
+    """.format(orders=orders_str)
+
+    marketing_material = frappe.db.sql(sql_query, as_dict=True)
+
+    return marketing_material
--- rubirosa/templates/pages/assets.js
+++ rubirosa/templates/pages/assets.js
@@ -1,3 +1,4 @@
+						
 document.addEventListener("DOMContentLoaded", function(event) {
     // process command line arguments
     get_arguments();
@@ -35,21 +36,56 @@ function loadPlatform(user) {
             user: user,
         },
         'callback': function (response) {
-            var user_info = response.message;
+			
+            var user_info = response.message.user_info;
+            var marketing_material = response.message.marketing_material;
+            var so_counter = 0;
+            var sinv_counter = 0;
             var user_orders = document.querySelector(".user-orders");
             var user_invoices = document.querySelector(".user-invoices");
+            
+            get_marketing_material(marketing_material);
             
             user_info.forEach(function (info, x) {
 				
 				if (info.sales_orders) {
-					user_orders.innerHTML += `<li class="list-group-item ">${info.sales_orders}</li>`
 					
-					user_invoices.innerHTML += `<li class="list-group-item ">SINV-TEST</li>`
+					so_counter = so_counter + 1;
+					user_orders.innerHTML += `<li class="list-group-item so-li">${info.sales_orders}</li>`;
+					
 				} else if (info.sales_invoices) {
-					user_invoices.innerHTML += `<li class="list-group-item ">${info.sales_invoices}</li>`
+					
+					sinv_counter = sinv_counter + 1;
+					user_invoices.innerHTML += `<li class="list-group-item sinv-li"> <a href="/api/method/erpnextswiss.erpnextswiss.guest_print.get_pdf_as_guest?doctype=Sales Invoice&name=${info.sales_invoices}&format=rubirosa Sales Invoice (consolidated)&no_letterhead=0" target="_blank" class="">${info.sales_invoices}</a></li>`;
+					
 				}
 				
 			});
 		}
 	});
+}
+
+function get_marketing_material(mm) {
+			
+	// List to store unique seasons
+	var unique_seasons = [];
+	var mm_counter = 0;
+	var materialli = document.querySelector(".material");
+		
+	mm.forEach(function (material, x) {
+			
+		if (!unique_seasons.includes(material.season)) {
+					
+			unique_seasons.push(material.season);
+			console.log("unique_seasons", unique_seasons)
+			if (material.image) {
+				mm_counter = mm_counter + 1;
+				materialli.innerHTML += `<li class="list-group-item marketingli"><img class='marketingImage' src="${material.image}"/></li>`;
+				console.log("material", materialli)
+				}
+		}
+	});
+			
+
+	
 }
