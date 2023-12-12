fadfb6823d75fefb70b7cbf6258b768a03b0c351
lasalesi <larsericmueller@gmail.com>
2023-12-12 16:49:40 +0100

    bugfix marketing material

--- rubirosa/rubirosa/doctype/marketing_material/marketing_material.js
+++ rubirosa/rubirosa/doctype/marketing_material/marketing_material.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Marketing Material', {
-	refresh: function(frm) {
+    refresh: function(frm) {
         // Display only items whose variants end with "420"
         frm.fields_dict['item_code'].get_query = function(doc, cdt, cdn) {
             return {
@@ -13,45 +13,32 @@ frappe.ui.form.on('Marketing Material', {
         };
         //Push Notification Item 
         frm.add_custom_button(__("Push Notifications Item"), function() {
-			push_notifications(frm, "Item");
-			
+            push_notifications(frm, "Item");
+            
         });
         
         //Push Notification Sales Season 
         frm.add_custom_button(__("Push Notifications Sales Season "), function() {
-			push_notifications(frm, "Season");
-			
+            push_notifications(frm, "Season");
+            
         });
-        
-        
-    },
-    after_save: function (frm) {
-
-		frappe.call({
-			'method': "rubirosa.rubirosa.doctype.marketing_material.marketing_material.fix_attachment",
-			'args': {
-				'name': frm.doc.name,
-				'url': frm.doc.image
-			}
-		});
-		
-	}
-    
+    }
 });
 
 function push_notifications(frm, reason) {
-	frappe.call({
-		'method': "rubirosa.rubirosa.doctype.marketing_material.marketing_material.push_notifications",
-		'args': {
-			'reason': reason,
-			'item': frm.doc.item_code,
-			'season': frm.doc.season
-		},
-		'callback': function (response) {
-			var contacts = response.message;
-			
-			window.location.href = "mailto:" + frm.doc.owner + "?subject=New Marketing Material&bcc=" + contacts.join(',');
+    frappe.call({
+        'method': "push_notifications",
+        'doc': frm.doc,
+        'args': {
+            'reason': reason,
+            'item': frm.doc.item_code,
+            'season': frm.doc.season
+        },
+        'callback': function (response) {
+            var contacts = response.message;
+            
+            window.location.href = "mailto:" + frm.doc.owner + "?subject=New Marketing Material&bcc=" + contacts.join(',');
 
-		}
-	});
+        }
+    });
 }
--- rubirosa/rubirosa/doctype/marketing_material/marketing_material.py
+++ rubirosa/rubirosa/doctype/marketing_material/marketing_material.py
@@ -7,46 +7,53 @@ import frappe
 from frappe.model.document import Document
 
 class MarketingMaterial(Document):
-    pass
-
-@frappe.whitelist()
-def fix_attachment(name, url):
-    # ~ Making sure that the attachment_image gets attach when is a new doc
-    file_doc = frappe.get_all("File", filters=[["attached_to_doctype", "=", "Marketing Material"], ["file_url", "=", url], ["attached_to_name", "like", "New Marketing Material%"]])
-    if len(file_doc) > 0:
-         doc = frappe.get_doc("File", file_doc[0].name)
-         frappe.db.set(doc, 'attached_to_name', name)
-         doc.save()
-    
-@frappe.whitelist()
-def push_notifications(reason=None, item=None, season=None):
-    reason_sql = None
-    if reason == "Item":
-        reason_sql = """ '{item}' LIKE CONCAT(LEFT(`tabSales Order Item`.`item_code`, LENGTH(`tabSales Order Item`.`item_code`) - 6), '%') """.format(item=item)
-    elif reason == "Season":
-        reason_sql = """ `tabSales Order`.`sales_season` = '{season}' """.format(season=season)
+    def after_insert(self):
+        if self.image:
+            self-fix_attachment()
+        return
         
-    sql_query = """
-        SELECT 
-            `tabUser`.`email`
-        FROM `tabContact`
-        LEFT JOIN `tabDynamic Link` ON `tabContact`.`name` = `tabDynamic Link`.`parent`
-            AND `tabDynamic Link`.`link_doctype` = "Customer"
-        LEFT JOIN `tabSales Order` ON `tabDynamic Link`.`link_name` = `tabSales Order`.`customer` 
-        LEFT JOIN `tabSales Order Item` ON `tabSales Order Item`.`parent` = `tabSales Order`.`name`
-        LEFT JOIN `tabUser` ON `tabContact`.`email_id` = `tabUser`.`email`
-        WHERE {reason_sql}
-        AND `tabSales Order`.`contact_person` = `tabContact`.`name`
-        AND `tabUser`.`name` IS NOT NULL
-        AND `tabContact`.`unsubscribed` = 0
-        GROUP BY `tabUser`.`name`
-        ORDER BY `tabSales Order`.`creation` DESC
-    """.format(reason_sql=reason_sql)
+    def fix_attachment(self):
+        # ~ Making sure that the attachment_image gets attach when is a new doc
+        file_doc = frappe.get_all("File", 
+            filters=[
+                ["attached_to_doctype", "=", "Marketing Material"], 
+                ["file_url", "=", self.image], 
+                ["attached_to_name", "LIKE", "New Marketing Material%"]
+            ])
+        if len(file_doc) > 0:
+             doc = frappe.get_doc("File", file_doc[0].name)
+             frappe.db.set(doc, 'attached_to_name', name)
+             doc.save()
+        return
     
-    contacts = frappe.db.sql(sql_query, as_dict=True)
-    users_email = []
+    def push_notifications(self, reason=None, item=None, season=None):
+        reason_sql = None
+        if reason == "Item":
+            reason_sql = """ '{item}' LIKE CONCAT(LEFT(`tabSales Order Item`.`item_code`, LENGTH(`tabSales Order Item`.`item_code`) - 6), '%') """.format(item=item)
+        elif reason == "Season":
+            reason_sql = """ `tabSales Order`.`sales_season` = '{season}' """.format(season=season)
+            
+        sql_query = """
+            SELECT 
+                `tabUser`.`email`
+            FROM `tabContact`
+            LEFT JOIN `tabDynamic Link` ON `tabContact`.`name` = `tabDynamic Link`.`parent`
+                AND `tabDynamic Link`.`link_doctype` = "Customer"
+            LEFT JOIN `tabSales Order` ON `tabDynamic Link`.`link_name` = `tabSales Order`.`customer` 
+            LEFT JOIN `tabSales Order Item` ON `tabSales Order Item`.`parent` = `tabSales Order`.`name`
+            LEFT JOIN `tabUser` ON `tabContact`.`email_id` = `tabUser`.`email`
+            WHERE {reason_sql}
+            AND `tabSales Order`.`contact_person` = `tabContact`.`name`
+            AND `tabUser`.`name` IS NOT NULL
+            AND `tabContact`.`unsubscribed` = 0
+            GROUP BY `tabUser`.`name`
+            ORDER BY `tabSales Order`.`creation` DESC
+        """.format(reason_sql=reason_sql)
+        
+        contacts = frappe.db.sql(sql_query, as_dict=True)
+        users_email = []
 
-    for contact in contacts:
-        users_email.append(contact.email)
+        for contact in contacts:
+            users_email.append(contact.email)
 
-    return users_email
+        return users_email
