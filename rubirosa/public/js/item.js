f76ae7ecc57f9ff464abce7afc664c294ef18bcd
lasalesi <larsericmueller@gmail.com>
2024-01-02 13:59:01 +0100

    disable ms direct item interface

--- rubirosa/public/js/item.js
+++ rubirosa/public/js/item.js
@@ -1,11 +1,12 @@
 frappe.ui.form.on("Item", {
     before_save: function(frm) {
-      if ((!frm.doc.__islocal) && (frm.doc.is_stock_item) && (frm.doc.barcode) && (frm.doc.barcode.length <= 17)) {
+      /* if ((!frm.doc.__islocal) && (frm.doc.is_stock_item) && (frm.doc.barcode) && (frm.doc.barcode.length <= 17)) {
           write_to_msdirect(frm);
-      }
+      } */
     }
 });
 
+/*
 function write_to_msdirect(frm) {
     // wait for the data to be stored in the database
     sleep(5000).then(() => {
@@ -27,3 +28,4 @@ function write_to_msdirect(frm) {
 function sleep (time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
+*/
