05877ce3788e209cc90c8cd136c0e485ac26ba63
lasalesi <larsericmueller@gmail.com>
2024-01-24 19:42:26 +0100

    new customer visit report feature

--- rubirosa/public/js/customer.js
+++ rubirosa/public/js/customer.js
@@ -1,3 +1,12 @@
+cur_frm.dashboard.add_transactions([
+    { 
+        'label': __('Documents'),
+        'items': [
+            'Customer Visit Report'
+        ]
+    }
+]);
+
 frappe.ui.form.on("Customer", {
     refresh: function(frm) {
         frm.add_custom_button(__("Statistics"), function() {
